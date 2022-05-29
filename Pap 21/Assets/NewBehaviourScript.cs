9ee33bce45b739be993f59b6bcdca8cec16c66c4
GarotoChamas <87251562+GarotoChamas@users.noreply.github.com>
2022-05-29 15:04:36 -0300

    iu9u899u98u89u989u
    vghuyyugygygkjiiuhgyyguiuguiguigigiuyguigiuguigguiguiguiuiguuguiguiugiguiugiugiugihuyhuy9u9y789y89y89y89y98y98y89y89y89yy89y88hy98y8y98y98y8y9y88y98yy889y8y8

---
+++ Pap 21/Assets/NewBehaviourScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public void btn(){
+         transform.Translate(-0.5f, 0, 0);
+    }
+
+
+    
+}
